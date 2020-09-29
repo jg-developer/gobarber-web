413d81cad1351173085d392d1278b18bd3d2c566
jg-developer <jonathan.gomes213@gmail.com>
2020-09-29 00:12:36 -0300

    create components button and input and signUp page

--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
-import signInBackgroundImage from '../../assets/sign-in-background.png';
+import signUpBackgroundImage from '../../assets/sign-up-background.png';
 
 export const Container = styled.div`
   height: 100vh;
@@ -41,7 +41,7 @@ export const Content = styled.div`
   }
 
   > a {
-    color: #ff9000;
+    color: #f4ede8;
     text-decoration: none;
     transition: color 0.2s;
     margin-top: 24px;
@@ -50,7 +50,7 @@ export const Content = styled.div`
     align-items: center;
 
     &:hover {
-      color: ${shade(0.2, '#ff9000')};
+      color: ${shade(0.2, '#F4EDE8')};
     }
 
     svg {
@@ -61,6 +61,6 @@ export const Content = styled.div`
 
 export const Background = styled.div`
   flex: 1;
-  background: url(${signInBackgroundImage}) no-repeat center;
+  background: url(${signUpBackgroundImage}) no-repeat center;
   background-size: cover;
 `;
